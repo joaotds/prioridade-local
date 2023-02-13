1ead5d2acbb4386500c9475b6b16e098013a3846
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-02-13 11:46:22 -0300

    Create prioridade.js

---
+++ prioridade.js
@@ -0,0 +1,10 @@
+//É possível ter as variáveis local e global com o mesmo nome. Quando você faz isso, a variável local tem precedência sobre a variável global.
+
+const someVar = "Hat";
+
+function myFun() {
+  const someVar = "Head";
+  return someVar;
+}
+
+//A função myFun retornará a string Head porque a versão local da variável está presente.
